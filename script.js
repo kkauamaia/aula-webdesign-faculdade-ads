20a3e0dcea9bfc5c46676a9d22a818bf92bdf200
kkauamaia <kauamaia2015@gmail.com>
2025-03-26 21:04:25 -0300

    removi um comentario do js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function changeFont(font) {
     document.body.style.fontFamily = font;
 }
 
-// Suaviza a rolagem ao clicar nas abas do menu
 document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function(event) {
         event.preventDefault();
